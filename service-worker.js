8bd43cb1c9ff53efcc0a013260bb34ee0a78f4df
urtzii2007 <urtzii2007@gmail.com>
2025-11-03 21:48:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,24 @@
-const CACHE = "desktop-v2";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
-];
+const CACHE = "desktop-v3";
+const ASSETS = ["./","./index.html","./style.css","./script.js","./manifest.json"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener("activate", e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  const req = e.request;
+self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(req).then(res => res || fetch(req).then(r => {
-      const rClone = r.clone();
-      caches.open(CACHE).then(c => c.put(req, rClone)).catch(()=>{});
-      return r;
-    }).catch(()=>res))
+    caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+      const clone = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, clone)).catch(()=>{});
+      return res;
+    }).catch(()=>r))
   );
 });
