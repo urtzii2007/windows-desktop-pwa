7b9b951bf1cd6b486b9fb876964270aefc6d536e
urtzii2007 <urtzii2007@gmail.com>
2025-11-03 20:54:09 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,33 @@
+const CACHE = "desktop-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
 self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
   e.waitUntil(
-    caches.open("desktop-cache").then(cache => {
-      return cache.addAll(["/", "/index.html", "/style.css", "/script.js", "/manifest.json"]);
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
+  const req = e.request;
   e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+    caches.match(req).then(res => res || fetch(req).then(r => {
+      const rClone = r.clone();
+      caches.open(CACHE).then(c => c.put(req, rClone)).catch(()=>{});
+      return r;
+    }).catch(()=>res))
   );
 });
